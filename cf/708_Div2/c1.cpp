9ee5586cd3fba7be307bc6bac10ebf734cca0dae
integralz <luckystay12@gmail.com>
2021-03-18 11:09:20 +0900

    div2

---
+++ cf/708_Div2/c1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <string>
+using namespace std;
+using ll = long long;
+
+void solve(){
+    int n, k;
+    scanf("%d%d", &n, &k);
+    if(n % 2 == 1){
+        printf("%d %d %d\n", n/2,n/2, 1);
+    }
+    else{
+        int a= n / 2;
+        if(a % 2 == 0){
+            printf("%d %d %d\n", a, a/2, a/2);
+        }
+        else{
+            printf("%d %d %d\n", a - 1, a - 1, 2);
+        }
+    }
+}
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    while(n--){
+        solve();
+    }
+}
+
+
+
