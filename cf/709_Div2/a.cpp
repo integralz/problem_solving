930a113d7ea09aa6a2fc6cca309c656a6ccf67f2
integralz <luckystay12@gmail.com>
2021-03-22 23:16:12 +0900

    div2

---
+++ cf/709_Div2/a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <string>
+using namespace std;
+using ll = long long;
+
+void solve(){
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a*b);
+}
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    while(n--){
+        solve();
+    }
+}
+
+
+
